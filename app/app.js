920a3615cf1866efac8c9e3d558aa710d6515966
PROGRESS\dedimitr <denitsa.dimitrova@progress.com>
2017-06-09 14:42:54 +0300

    Subscribe to the resume event #344634

--- app/app.js
+++ app/app.js
@@ -1,3 +1,16 @@
 var application = require("application");
 application.setCssFileName("./app.css");
 application.start({ moduleName: "main-page" });
+
+application.on(application.resumeEvent, function(args) {
+    if (args.android) {
+        var act = args.android;
+        var intent = act.getIntent();
+        var extras = intent.getExtras();
+        if (extras) {
+            var myKeyValue = extras.get('myKey');
+            console.log('myKey: ' + myKeyValue);
+            intent.removeExtra('myKey');
+        }
+    }
+});
